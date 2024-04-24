69a255de4a08749b24e39d1a229af3d7cba90a34
Louis <louisc00@proton.me>
2024-04-24 17:23:39 -0600

    reset the fetch, cause it's chat app. and seperate the typeDefs and resolvers file

---
+++ typeDefs.js
@@ -0,0 +1,24 @@
+const typeDefs = `#graphql
+  type Query {
+    greet: String,
+    users: [User],
+    user(id: ID): User
+  }
+
+  type User {
+    id: ID
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+  }
+
+  input UserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+  }
+`;
+
+export default typeDefs;
