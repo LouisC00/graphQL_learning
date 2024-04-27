1b3c3921c87915214d7e0b7de23c1f89a65c15b1
Louis <louisc00@proton.me>
2024-04-26 20:01:06 -0600

    add chat screen

--- client/src/pages/HomeScreen.js
+++ client/src/pages/HomeScreen.js
@@ -3,12 +3,13 @@ import { Box } from "@mui/material";
 import SideBar from "../components/SideBar";
 import { Route, Routes } from "react-router-dom";
 import Welcome from "../components/Welcome";
+import ChatScreen from "../components/ChatScreen";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
-      <Route path="/:id/:name" element={<Welcome />} />
+      <Route path="/:id/:name" element={<ChatScreen />} />
     </Routes>
   );
 };
