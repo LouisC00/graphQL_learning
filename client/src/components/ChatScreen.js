3643fdb6a90cea7551cd71b0237a3e1a15287e7f
Louis <louisc00@proton.me>
2024-04-27 16:27:03 -0600

    showing message

--- client/src/components/ChatScreen.js
+++ client/src/components/ChatScreen.js
@@ -1,12 +1,49 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Avatar,
+  Typography,
+  TextField,
+} from "@mui/material";
+import MessageCard from "./MessageCard";
 
 const ChatScreen = () => {
   const { id, name } = useParams();
   return (
-    <div>
-      {id} {name}
-    </div>
+    <Box flexGrow={1}>
+      <AppBar position="static" sx={{ backgroundColor: "white", boxShadow: 0 }}>
+        <Toolbar>
+          <Avatar
+            src={`https://api.dicebear.com/8.x/initials/svg?seed=${name}`}
+            sx={{ width: "32px", height: "32px", mr: 2 }}
+            alt="avatar"
+          />
+          <Typography varaint="h6" color="black">
+            {name}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        backgroundColor="#f5f5f5"
+        height="80vh"
+        padding="10px"
+        sx={{ overflowY: "auto" }}
+      >
+        <MessageCard text="hi paxu" date="1234" direction="end" />
+        <MessageCard text="hi paxu" date="1234" />
+        <MessageCard text="hi paxu" date="1234" />
+      </Box>
+      <TextField
+        placeholder="Enter a message"
+        variant="standard"
+        fullWidth
+        multiline
+        rows={2}
+      />
+    </Box>
   );
 };
 
