d204402f61c78b19aaa83805fd59725931ab84dc
Louis <louisc00@proton.me>
2024-04-27 16:27:14 -0600

    add logout button

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Box, Typography, Divider } from "@mui/material";
+import { Box, Typography, Divider, Stack } from "@mui/material";
 import UserCard from "./UserCard";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const SideBar = () => {
   const users = [
@@ -11,7 +12,10 @@ const SideBar = () => {
 
   return (
     <Box backgroundColor="#f7f7f7" height="100vh" width="250px" padding="0.8vw">
-      <Typography variant="h6">Chat</Typography>
+      <Stack direction="row" justifyContent="space-between">
+        <Typography variant="h6">Chat</Typography>
+        <LogoutIcon />
+      </Stack>
       <Divider />
       {users.map((item) => {
         return <UserCard key={item.id} item={item} />;
