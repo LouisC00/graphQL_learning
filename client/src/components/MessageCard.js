3d8f0fa74b43ac3b14b6b503600d5241f6318ff6
Louis <louisc00@proton.me>
2024-04-28 16:13:07 -0600

    format created Time

--- client/src/components/MessageCard.js
+++ client/src/components/MessageCard.js
@@ -8,7 +8,9 @@ const MessageCard = ({ text, date, direction }) => {
         <Typography variant="subtitle1" backgroundColor="white" padding="5px">
           {text}
         </Typography>
-        <Typography variant="caption">{date}</Typography>
+        <Typography variant="caption">
+          {new Date(date).toLocaleTimeString()}
+        </Typography>
       </Box>
     </Box>
   );
