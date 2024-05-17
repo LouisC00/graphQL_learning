ab63acc789d2adff9da8f1972fce4deecbc5fc6a
Louis <louisc00@proton.me>
2024-05-17 15:19:46 -0600

    auto logout if there's no that user on database

--- client/src/pages/HomeScreen.js
+++ client/src/pages/HomeScreen.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 import SideBar from "../components/SideBar";
 import { Route, Routes } from "react-router-dom";
 import Welcome from "../components/Welcome";
 import ChatScreen from "../components/ChatScreen";
+import { useQuery } from "@apollo/client";
+import { GET_CURRENT_USER_STATUS } from "../graphql/queries";
 
 const AllRoutes = () => {
   return (
@@ -15,6 +17,16 @@ const AllRoutes = () => {
 };
 
 const HomeScreen = ({ setLoggedIn }) => {
+  useQuery(GET_CURRENT_USER_STATUS, {
+    onError: (error) => {
+      console.error("Error fetching user status:", error);
+      if (error.message.includes("User not found")) {
+        localStorage.removeItem("jwt");
+        setLoggedIn(false);
+      }
+    },
+  });
+
   return (
     <Box display="flex">
       <SideBar setLoggedIn={setLoggedIn} />
